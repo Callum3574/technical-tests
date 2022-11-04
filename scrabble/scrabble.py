d17bdb195c2028772be5ea1a541c0ddb7bce37e7
Callum Hall <callumhall@Callums-MacBook-Pro.local>
2022-11-04 13:40:40 +0000

    35 mins in

--- scrabble/scrabble.py
+++ scrabble/scrabble.py
@@ -1,8 +1,8 @@
 import random
+import collections
 
-
-with open ('dictionary.txt') as f:
-    contents = f.read()
+# with open ('dictionary.txt') as f:
+#     contents = f.read()
 
 alphabet = {
     'a':1,
@@ -56,5 +56,23 @@ def return_7_tiles(bag):
     for i in range(7):
         player_bag.append(bag.pop())
     return player_bag
+
+tiles = return_7_tiles(bag)
+tiles_in_string = ''.join(tiles).lower()
+
+
+#compare given tiles to words in dict - aim here is to have a counter of each letter that the player has. If this amount of is present in a word in dict, return the word. 
+#I've found a collections import, creates dictionary with each char and amount of times it's present
+
+#this returns a dict of all the char as keys, and value of how many times present. ----> The aim here is to compare this to each word in the dict
+letters = collections.Counter(tiles_in_string)
+
+
+#need to loop through the dict and make a collection dict
+#this loops through line of text file and return the word as a dict similar to letters above... 
+for line in open('dictionary.txt'):
+    dict_words = collections.Counter(line)
+    print(dict_words)
+
 
 
